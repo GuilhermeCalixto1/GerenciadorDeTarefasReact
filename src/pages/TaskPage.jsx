eb998ffa14e572cbeca6f49fdc321c76441734f6
GuilhermeCalixto1 <guilhermegbm.gc@gmail.com>
2026-02-15 19:10:22 -0300

    feat: Redesenhar página de detalhes da tarefa com layout responsivo e botão de voltar

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,14 +1,35 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function TaskPage() {
-  const [searchParams] = useSearchParams();
-  const title = searchParams.get("title");
-  const description = searchParams.get("description");
+  const location = useLocation();
+  const { title, description } = location.state || {};
+  const navigate = useNavigate();
 
   return (
-    <div className="h-screen w-screen bg-slate-500 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+    // ADICIONADO: flex e justify-center para alinhar o card no meio da tela
+    <div className="h-screen w-screen bg-slate-500 p-6 flex justify-center">
+      <div className="w-[500px] space-y-4">
+        {/* Cabeçalho: Título no centro, botão na esquerda */}
+        <div className="flex justify-center relative mb-6">
+          <button
+            onClick={() => navigate(-1)} // Alterado para -1 (ver explicação abaixo)
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da tarefa
+          </h1>
+        </div>
+
+        {/* Descrições */}
+        <div className="bg-slate-200 rounded-md p-4">
+          <h2 className="text-slate-600 font-bold text-xl">{title}</h2>
+          <p className="text-slate-600 mt-2">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
